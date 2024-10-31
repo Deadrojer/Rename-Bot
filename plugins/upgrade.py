02e95fcfdeee893583765bb984e69cea31dcffc2
Deadrojer <139552280+Deadrojer@users.noreply.github.com>
2024-10-31 12:06:21 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -6,29 +6,29 @@ from pyrogram import Client , filters
 
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
-    text = """**Free Plan User**
+    text = """Free Plan User
 Daily  Upload limit 5GB
 Price 0
 
-**🪙 Basic**
+🪙 Basic
 Daily  Upload  limit 20GB
-Price Rs 49  ind /🌎 0.59$  per Month
+Price Rs 39  ind /🌎 0.49$  per Month
 
-**⚡ Standard**
+⚡ Standard
 Daily Upload limit 50GB
-Price Rs 99  ind /🌎 1.19$  per Month
+Price Rs 80  ind /🌎 1$  per Month
 
-**💎 Pro**
+💎 Pro
 Daily Upload limit 100GB
-Price Rs 179  ind /🌎 2.16$  per Month
+Price Rs 150  ind /🌎 2$  per Month
 
 Payment Details :-
-<b>➜ UPI ID :</b> <code>RahulReviews@UPI</code>
+<b>➜ UPI ID :</b> <code>narutoprit@fam</code>
 
-After Payment Send Screenshots Of Payment To Admin @CodeXBro"""
+After Payment Send Screenshots Of Payment To Admin @NARUTO_UZUMAKI07th""
     
     keybord = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin", url = "https://telegram.me/CodeXBro"),
+        [InlineKeyboardButton("🦋 Admin", url = "https://telegram.me/NARUTO_UZUMAKI07th"),
         InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
         ])
     
@@ -38,29 +38,29 @@ After Payment Send Screenshots Of Payment To Admin @CodeXBro"""
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-    text = """**Free Plan User**
+    text = """Free Plan User
 Daily  Upload limit 5GB
 Price 0
 
-**🪙 Basic**
+🪙 Basic
 Daily  Upload  limit 20GB
-Price Rs 49  ind /🌎 0.59$  per Month
+Price Rs 39  ind /🌎 0.49$  per Month
 
-**⚡ Standard**
+⚡ Standard
 Daily Upload limit 50GB
-Price Rs 99  ind /🌎 1.19$  per Month
+Price Rs 80  ind /🌎 1$  per Month
 
-**💎 Pro**
+💎 Pro
 Daily Upload limit 100GB
-Price Rs 179  ind /🌎 2.16$  per Month
+Price Rs 150  ind /🌎 2$  per Month
 
 Payment Details :-
-<b>➜ UPI ID :</b> <code>RahulReviews@UPI</code>
+<b>➜ UPI ID :</b> <code>Narutoprit@fam</code>
 
-After Payment Send Screenshots Of Payment To Admin @CodeXBro"""
+After Payment Send Screenshots Of Payment To Admin @NARUTO_UZUMAKI07th"""
     
     keybord = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin", url = "https://telegram.me/CodeXBro"),
+        [InlineKeyboardButton("🦋 Admin", url = "T.me/NARUTO_UZUMAKI07th"),
         InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
         ])
     
